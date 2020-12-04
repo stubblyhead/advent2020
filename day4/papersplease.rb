8707c89659b8177c5723f72c6cde6c8a3c9d6c96
Mike Stevenson <miksteve@amazon.com>
2020-12-04 13:22:39 -0800

    fixed wrong variable, but logic is still wrong somewhere

--- day4/papersplease.rb
+++ day4/papersplease.rb
@@ -5,10 +5,14 @@ validcount = 0
 papers.each do |i|
   pp = i.split
   if pp.length == 8
+    puts "#{pp} is valid, all fields present"
     validcount += 1 #if all fields are present it's a valid passport
   elsif pp.length == 7
    pp.sort!
-    validcount += 1 if i[1][0..2] == 'ecl' #only cid can be missing, so the second element has to start with 'ecl'
+   if pp[1][0..2] == 'ecl'  #only cid can be missing, so the second element has to start with 'ecl'
+     validcount += 1
+     puts "#{pp} is valid, only cid is missing"
+   end
   end
 end
 

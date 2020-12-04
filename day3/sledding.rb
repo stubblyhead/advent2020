64247518ee7b055a5ac5804691cb7e7e3875cf90
Mike Stevenson <miksteve@amazon.com>
2020-12-03 16:34:22 -0800

    first try part 2

--- day3/sledding.rb
+++ day3/sledding.rb
@@ -12,3 +12,19 @@ until y == lines.length - 1 do
 end
 
 puts treecount
+
+[[1,1],[5,1],[7,1],[1,2]].each do |i|
+  x = 0
+  y = 0
+  thiscount = 0
+  x_inc = i[0]
+  y_inc = i[1]
+  until y >= lines.length - 1 do
+    x += x_inc
+    y += y_inc
+    thiscount += 1 if lines[y][x%width] == '#'
+  end
+  treecount *= thiscount
+end
+
+puts treecount

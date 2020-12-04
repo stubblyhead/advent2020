d4f8e0d0c29fa7b6566ebfd47dd86bc274a4f122
Mike Stevenson <miksteve@amazon.com>
2020-12-04 14:46:52 -0800

    sometimes the first field is missing

--- day4/papersplease.rb
+++ day4/papersplease.rb
@@ -5,13 +5,13 @@ validcount = 0
 papers.each do |i|
   pp = i.split
   if pp.length == 8
-    puts "#{pp} is valid, all fields present"
     validcount += 1 #if all fields are present it's a valid passport
   elsif pp.length == 7
    pp.sort!
-   if pp[1][0..2] == 'ecl'  #only cid can be missing, so the second element has to start with 'ecl'
+   if pp[0][0..2] == 'byr' && pp[1][0..2] == 'ecl'
+     #only cid can be missing, so the first element has to start with 'byr,
+     #and the second element has to start with 'ecl'
      validcount += 1
-     puts "#{pp} is valid, only cid is missing"
    end
   end
 end

162f3ea9c6b2ee733c61493e93fd055bf7bc6e9b
Mike Stevenson <miksteve@amazon.com>
2020-12-04 13:10:49 -0800

    split doesn't change the object, also not finding some valid passports

--- day4/papersplease.rb
+++ day4/papersplease.rb
@@ -3,11 +3,11 @@ papers = File.readlines('./input', sep="\n\n", :chomp=>true)
 validcount = 0
 
 papers.each do |i|
-  i.split
-  if i.length == 8
+  pp = i.split
+  if pp.length == 8
     validcount += 1 #if all fields are present it's a valid passport
-  elsif i.length == 7
-    i.sort!
+  elsif pp.length == 7
+   pp.sort!
     validcount += 1 if i[1][0..2] == 'ecl' #only cid can be missing, so the second element has to start with 'ecl'
   end
 end

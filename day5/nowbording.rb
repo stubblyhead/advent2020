2f974148133e316de6a4a78da27feeb803bbb712
Mike Stevenson <miksteve@amazon.com>
2020-12-06 12:08:50 -0800

    missing something on part 2

--- day5/nowbording.rb
+++ day5/nowbording.rb
@@ -1,15 +1,22 @@
 passes = File.readlines('./input', :chomp=>true)
 
-max_id = 0
+id_list = []
 passes.each do |i|
-  row = i[0,7]
+  row = i[0,7]  #splitting these off wasn't necessary but oh well
   seat = i[-3,3]
   row.gsub!('B','1')
   row.gsub!('F','0')
   seat.gsub!('R','1')
   seat.gsub!('L','0')
   seat_id = row.to_i(2) * 8 + seat.to_i(2)
-  max_id = [max_id, seat_id].max
+  id_list.push(seat_id)
 end
 
-puts max_id
+puts id_list.max
+id_list.sort
+id_list.each_index do |i|
+  if id_list[i+1] - id_list[i] == 2
+    puts id_list[i] + 1
+    exit
+  end
+end

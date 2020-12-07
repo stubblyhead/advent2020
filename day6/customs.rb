cf07416bf8520315bcf9a3b5caa7dcd41c6cc0b6
Mike Stevenson <miksteve@amazon.com>
2020-12-06 16:04:37 -0800

    readline's chomp wasn't chomping on the last element for some reason

--- day6/customs.rb
+++ day6/customs.rb
@@ -1,4 +1,5 @@
 answers = File.readlines('./input', sep = "\n\n", :chomp => true)
+answers[-1].chomp! #not sure why this isn't getting chomped already, but it's not
 
 count = 0
 answers.each do |i|
@@ -13,7 +14,10 @@ answers.each do |i|
   groupsize = i.count("\n") + 1  #newlines + 1 is number of people in group
   yes_questions = i.gsub("\n",'').split('').uniq #get list of answers with at least one yes
   yes_questions.each do |a|
-    count += 1 if i.count(a) == groupsize #increment count if the yes answer appears as many times as the group is large
+    if i.count(a) == groupsize #increment count if the yes answer appears as many times as the group is large
+      all_yes += a
+      count += 1
+    end
   end
 end
 

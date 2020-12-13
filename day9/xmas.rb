46bda04db8ccf56b49209dd7ba67d0a3ece7a9ae
Mike Stevenson <miksteve@amazon.com>
2020-12-13 09:16:44 -0800

    first clunky pass at part 2, runs to quickly to be doing anything though

--- day9/xmas.rb
+++ day9/xmas.rb
@@ -1,17 +1,28 @@
-numbers = File.readlines('./testcase', :chomp => true).map { |i| i.to_i }
+numbers = File.readlines('./input', :chomp => true).map { |i| i.to_i }
 
 target = nil
 numbers.each_index do |i|
-  next if i < 5  #i could do this more elegantly
+  next if i < 25  #i could do this more elegantly
 
-  searchspace = numbers[i-5,5]  #get the previous x numbers to search
+  searchspace = numbers[i-25,25]  #get the previous x numbers to search
   searchspace.each_index do |j|
     next if searchspace[j] * 2 == numbers[i]  #the two numbers have to be different, so go to the next if it's exactly half
     target = numbers[i] - searchspace[j]
     break if searchspace.index(target)  #if the search space has the conpmlement then we can move on
     if j == searchspace.length - 1
-      puts numbers[i] #we should only hit this line if the number isn't the sum of any 2 of the previous 5
+      puts weak = numbers[i] #we should only hit this line if the number isn't the sum of any 2 of the previous 5
       exit
+    end
+  end
+end
+
+numbers.each_index do |i|
+  numbers.length.times do |j|
+    sum = numbers[i..j].sum
+    if sum == weak
+      return numbers[i..j].min + numbers[i..j].max
+    elsif sum > weak
+      break
     end
   end
 end

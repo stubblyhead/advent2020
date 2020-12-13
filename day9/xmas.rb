22e04cb6e7dc967a0b71d7b39dbc3fc07b9a49bb
Mike Stevenson <miksteve@amazon.com>
2020-12-13 09:53:03 -0800

    part 2 complete

--- day9/xmas.rb
+++ day9/xmas.rb
@@ -1,6 +1,7 @@
 numbers = File.readlines('./input', :chomp => true).map { |i| i.to_i }
 
 target = nil
+weak = nil
 numbers.each_index do |i|
   next if i < 25  #i could do this more elegantly
 
@@ -11,16 +12,18 @@ numbers.each_index do |i|
     break if searchspace.index(target)  #if the search space has the conpmlement then we can move on
     if j == searchspace.length - 1
       puts weak = numbers[i] #we should only hit this line if the number isn't the sum of any 2 of the previous 5
-      exit
+      break
     end
   end
 end
 
 numbers.each_index do |i|
   numbers.length.times do |j|
+    next if j <= i
     sum = numbers[i..j].sum
     if sum == weak
-      return numbers[i..j].min + numbers[i..j].max
+      puts numbers[i..j].min + numbers[i..j].max
+      exit
     elsif sum > weak
       break
     end

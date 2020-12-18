8315e7c47fdf972b48b3ce355ff01adf28c74d76
Mike Stevenson <miksteve@amazon.com>
2020-12-17 16:34:59 -0800

    passing test case

---
+++ day13/busstop.rb
@@ -0,0 +1,16 @@
+lines = File.readlines('./testcase', :chomp => true)
+time = lines[0].to_f #making this a float will make the calculations later more convenient
+routes = lines[1].split(',').map { |i| i.to_i }
+
+shortest_wait = 9999  #start with something suitably large
+answer = 0
+routes.each do |i|
+  next if i == 0
+  wait = ((time/i).ceil)*i - time
+  if wait < shortest_wait
+    shortest_wait = wait
+    answer = (shortest_wait * i).to_i
+  end
+end
+
+puts answer

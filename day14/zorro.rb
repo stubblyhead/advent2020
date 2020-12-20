659dc7629568e118ffe048e3f2979b33c783dfd1
Mike Stevenson <miksteve@amazon.com>
2020-12-19 19:42:30 -0800

    misread directions, you mask first then iterate not the other way around

--- day14/zorro.rb
+++ day14/zorro.rb
@@ -28,16 +28,37 @@ sum = 0
 mem.each { |i| sum += i if i}
 puts sum
 
-def build_masks(mem_mask)
+def calculate_addresses(mem_mask)
   x_loc = []
-  mask_list = []
+  addr_list = []
   mem_mask.split('').each_index { |i| x_loc.push(i) if mem_mask[i] == 'X' }
   (2**x_loc.length).times do |i|
-    this_mask = mem_mask.clone
+    this_addr = mem_mask.clone
     x_loc.length.times do |j|
-      i[j] == 1 ? this_mask[x_loc[j]] = '1' : this_mask[x_loc[j]] = '0'
+      i[j] == 1 ? this_addr[x_loc[j]] = '1' : this_addr[x_loc[j]] = '0'
     end
-    mask_list.push(this_mask.to_i(2))
+    addr_list.push(this_mask.to_i(2))
   end
-  return mask_list
+  return addr_list
+end
+
+lines.each do |i|
+  if i[0,4] == 'mask'
+    mask = i.split(' ')[2]
+    on_mask = mask.gsub('X','0').to_i(2)  #do a bitwise OR to turn bits on
+    off_mask = ''
+    mask.each_char { |c| c == '0'? off_mask += '1' : off_mask += '0' }
+    off_mask = ~(off_mask.to_i(2))
+  else
+    parts = i.split(' ')
+    addr = parts[0][4..].to_i
+    val = parts[2].to_i
+    addr = addr | on_mask
+    addr = addr & off_mask
+    addr = addr.to_s(2)
+    #now we need to put the X's back in
+    mask_bits = mask.reverse.split('')
+    mask_bits.each_index { |i| addr[i] = 'X' if mask_bits[i] == 'X' }
+    addrs = calculate_addresses(addr)
+    
 end

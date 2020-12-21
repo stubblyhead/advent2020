6ac61a50ce979c8073b67c5e7b9979d507a1033d
Mike Stevenson <miksteve@amazon.com>
2020-12-20 19:35:31 -0800

    new function to find bad values on tickets

--- day16/ticketsplease.rb
+++ day16/ticketsplease.rb
@@ -19,5 +19,16 @@ end
 my_ticket = lines[1].split("\n")[1].split(',').map { |i| i.to_i }
 other_tickets = lines[2].split("\n")[1..].map{|i| i.split(',').map { |j| j.to_i } }
 
-p my_ticket
-p other_tickets
+def valid(rules, ticket)
+  #returns 0 if ticket is valid, otherwise returns field that doesn't satisfy any rules
+  valid_fields = Hash.new { |hash,key| hash[key] = [] }
+  ticket.each do |val|
+    rules.each_key do |key|
+      rules[key].each do |r|
+        valid_fields(val).push(key) if r.covers?(val)
+      end
+    end
+  end
+  bad_vals = valid_fields.select { |k,v| v == [] }
+  bad_vals == [] ? return 0 : return[0]
+end
